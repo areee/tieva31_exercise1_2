f7dd7625cbf9836a340f7f594e85fb06a6674db6
areee <areee@users.noreply.github.com>
2017-03-10 14:31:53 +0200

    exercise UI started

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper'
+import TextField from 'material-ui/TextField'
+import Checkbox from 'material-ui/Checkbox'
+import RaisedButton from 'material-ui/RaisedButton'
 
 class App extends Component {
+
+
+
   render() {
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <Paper style={{width: 400, padding: 20}}>
+          Content
+        </Paper>
+      </MuiThemeProvider>
     );
   }
 }
