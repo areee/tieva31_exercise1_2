ff803102606f06d1b621a9e6425e812f04876a73
areee <areee@users.noreply.github.com>
2017-03-13 23:14:15 +0200

    UI is ready (see the ui_image)

--- src/App.js
+++ src/App.js
@@ -9,9 +9,31 @@ import TextField from 'material-ui/TextField'
 import Checkbox from 'material-ui/Checkbox'
 import RaisedButton from 'material-ui/RaisedButton'
 
-const styles = {
+/*const styles = {
   display: 'inline-block'
-}
+}*/
+
+const styles = {
+  div:{
+    display: 'flex',
+    flexDirection: 'row wrap',
+    padding: 20,
+    width: '10%'
+  },
+  paperLeft:{
+    flex: 1,
+    height: '100%',
+    margin: 10,
+    textAlign: 'center',
+    padding: 10
+  },
+  paperRight:{
+    height: 600,
+    flex: 4,
+    margin: 10,
+    textAlign: 'center',
+  }
+};
 
 class App extends Component {
 
@@ -21,28 +43,29 @@ class App extends Component {
 
     return (
       <MuiThemeProvider>
-        <Paper style={{width: 600, margin: 20, padding: 20}}>
-          <div style={{styles}}>
-            <Checkbox />
-            <Checkbox />
-            <Checkbox />
-            <Checkbox />
-            <Checkbox />
-            <Checkbox />
-            <Checkbox />
-            <Checkbox />
+        <Paper style={{width: 400, margin: 20, padding: 20}}>
+          <div style={styles.div}>
+            <Checkbox id="1" />
+            <Checkbox id="2" />
+            <Checkbox id="3" />
+            <Checkbox id="4" />
+            <Checkbox id="5" />
+            <Checkbox id="6" />
+            <Checkbox id="7" />
+            <Checkbox id="8" />
           </div>
-          <div>
+          <div style={{textAlign: 'center'}}>
             <TextField setValue={0} id="text field 1" />
           </div>
-          <div>
+          <div style={{textAlign: 'center'}}>
           <RaisedButton label="Convert" />
           </div>
-          <div>
+          <div style={{textAlign: 'center'}}>
             <TextField setValue={0} id="text field 2" />
           </div>
         </Paper>
       </MuiThemeProvider>
+      
     );
   }
 }
