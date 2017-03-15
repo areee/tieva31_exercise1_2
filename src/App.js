13372aaa56443408f9bbf756a7940fe2f43ea125
areee <areee@users.noreply.github.com>
2017-03-15 13:32:24 +0200

    exercise done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import TextField from 'material-ui/TextField'
 import Checkbox from 'material-ui/Checkbox'
 import RaisedButton from 'material-ui/RaisedButton'
 
-let result = "";
+var result = "00000000";
+var result2 = "xxxxxx";
+/*let index = 0;*/
 
 const styles = {
   div:{
@@ -19,6 +21,21 @@ const styles = {
     width: '10%'
   },
 };
+
+/*function replaceAt(str, index, character) {
+    return str.substr(0, index) + character + str.substr(index+character.length);
+}*/
+
+/*function setCharAt(str,index,chr) {
+    return str.substr(0,index) + chr + str.substr(index+1);
+}*/
+
+String.prototype.replaceAt=function(index, char) {
+    var a = this.split("");
+    a[index] = char;
+    return a.join("");
+}
+
 
 class App extends Component {
 
@@ -33,7 +50,7 @@ constructor(){
     cb6checked: 0,
     cb7checked: 0,
     cb8checked: 0,
-    tf1: "00000000",
+    tf1: result,
     tf2: "",
   }
 }
@@ -48,103 +65,143 @@ updateTf2 = (event) => {
 
 useCb1 = (event, isChecked) => {
   if(isChecked){
+    /*result = this.props.tf1;*/
+    /*result = replaceAt(result, 1,"1");*/
+    /*result = this.substr(0, 0) + "1" + this.substr(0+"1".length);*/
+    /*result = "asdf";*/
+    /*result = setCharAt(result,0,'1');*/
+    
+    result = result.replaceAt(0, "1");
     this.setState({
     cb1checked: 1,
-    
+    tf1: result,
   });
   }else{
+    result = result.replaceAt(0, "0");
     this.setState({
     cb1checked: 0,
+    tf1: result,
   });
   }
 }
 
 useCb2 = (event, isChecked) => {
   if(isChecked){
+    result = result.replaceAt(1, "1");
     this.setState({
     cb2checked: 1,
+    tf1: result,
   });
-  }else{
+}else{
+  result = result.replaceAt(1, "0");
     this.setState({
-    cb2checked: 0,
+      cb2checked: 0,
+      tf1: result,
   });
   }
 }
 
 useCb3 = (event, isChecked) => {
   if(isChecked){
+    result = result.replaceAt(2, "1");
     this.setState({
-    cb3checked: 1,
+      cb3checked: 1,
+      tf1: result,
   });
   }else{
+    result = result.replaceAt(2, "0");
     this.setState({
-    cb3checked: 0,
+      cb3checked: 0,
+      tf1: result,
   });
   }
 }
 
 useCb4 = (event, isChecked) => {
   if(isChecked){
+    result = result.replaceAt(3, "1");
     this.setState({
-    cb4checked: 1,
+      cb4checked: 1,
+      tf1: result,
   });
   }else{
+    result = result.replaceAt(3, "0");
     this.setState({
-    cb4checked: 0,
+      cb4checked: 0,
+      tf1: result,
   });
   }
 }
 
 useCb5 = (event, isChecked) => {
   if(isChecked){
+    result = result.replaceAt(4, "1");
     this.setState({
-    cb5checked: 1,
+      cb5checked: 1,
+      tf1: result,
   });
   }else{
+    result = result.replaceAt(4, "0");
     this.setState({
-    cb5checked: 0,
+      cb5checked: 0,
+      tf1: result,
   });
   }
 }
 
 useCb6 = (event, isChecked) => {
   if(isChecked){
+    result = result.replaceAt(5, "1");
     this.setState({
-    cb6checked: 1,
+      cb6checked: 1,
+      tf1: result,
   });
   }else{
+    result = result.replaceAt(5, "0");
     this.setState({
-    cb6checked: 0,
+      cb6checked: 0,
+      tf1: result,
   });
   }
 }
 
 useCb7 = (event, isChecked) => {
   if(isChecked){
+    result = result.replaceAt(6, "1");
     this.setState({
-    cb7checked: 1,
+      cb7checked: 1,
+      tf1: result,
   });
   }else{
+    result = result.replaceAt(6, "0");
     this.setState({
-    cb7checked: 0,
+      cb7checked: 0,
+      tf1: result,
   });
   }
 }
 
 useCb8 = (event, isChecked) => {
+  result = result.replaceAt(7, "1");
   if(isChecked){
     this.setState({
-    cb8checked: 1,
+      cb8checked: 1,
+      tf1: result,
   });
   }else{
+    result = result.replaceAt(7, "0");
     this.setState({
-    cb8checked: 0,
+      cb8checked: 0,
+      tf1: result,
   });
   }
 }
 
 convert = (event) => {
-
+  result2 = parseInt(result,2);
+  this.setState({
+      tf2: result2,
+  });
 }
 
   render() {
@@ -163,7 +220,7 @@ convert = (event) => {
             <Checkbox id="cb 8" onCheck={this.useCb8} />
           </div>
           <div style={{textAlign: 'center'}}>
-            <TextField value={this.state.tf1} id="tf 1" />
+            <TextField value={this.state.tf1} id="tf 1"/>
           </div>
           <div style={{textAlign: 'center'}}>
           <RaisedButton label="Convert" onClick={this.convert} />
